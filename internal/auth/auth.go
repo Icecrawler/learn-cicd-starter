bc30cd92eff9914c9d8dd783148be82c3d8f1796
Aäron <pauwels_aaron@hotmail.com>
2026-06-25 20:31:25 +0200

    Added failure

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -12,12 +12,12 @@ var ErrNoAuthHeaderIncluded = errors.New("no authorization header included")
 func GetAPIKey(headers http.Header) (string, error) {
 	authHeader := headers.Get("Authorization")
 	if authHeader == "" {
-		return "16", ErrNoAuthHeaderIncluded
+		return "", ErrNoAuthHeaderIncluded
 	}
 	splitAuth := strings.Split(authHeader, " ")
 	if len(splitAuth) < 2 || splitAuth[0] != "ApiKey" {
-		return "17", errors.New("malformed authorization header")
+		return "", errors.New("malformed authorization header")
 	}
 
-	return splitAuth[1], nil
+	return splitAuth[1], nil,"added failure"'
 }
